9ea171ed4ee8c31ae17f8039d402564615932a73
KelseyChetnik <kelsey.chetnik@gmail.com>
2020-12-14 16:31:52 -0500

    Add additional languages

--- goodbye.R
+++ goodbye.R
@@ -1,7 +1,13 @@
 # This is a function to say goodbye given a name
 
-say_goodbye <- function(name="Kelsey"){
+say_goodbye <- function(name="Kelsey", language="English"){
 
-  print(paste0("Goodbye, ", name, "!"))
+  if(language=="English"){
+    print(paste0("Goodbye, ", name, "!"))
+  }else if(language=="Spanish"){
+    print(paste0("Adios, ", name, "!"))
+  }else if(language=="Japanese"){
+    print(paste0("Sayounara, ", name, "!"))
+  }
 
 }
